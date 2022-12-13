d7d046011b258bf4ae6b616e4ad3289d968fb636
nhn1030 <shgusska12@gmail.com>
2022-12-14 00:25:41 +0900

    변수 자바스크립트 파일 만들기

--- java-script/day1/nhn/variable.js
+++ java-script/day1/nhn/variable.js
@@ -1,2 +1,14 @@
-name = '홍길동'
-age = 25
+//변수란? --> 하나의 값을 저장하기 위한 수단
+//값을 저장하기 위해선? --> 변수선언을 해야함
+
+var score //변수선언
+score = 10 // 값의 할당
+console.log(score) //10
+
+var score2 = 20 //변수선언과 값의 할당
+console.log(score2) //20
+
+var score3 = 30
+console.log(score3)
+score3 = 40 // 값의 재할당
+console.log(score3) // 이 시점에서 식별자 score3의 값은 40
