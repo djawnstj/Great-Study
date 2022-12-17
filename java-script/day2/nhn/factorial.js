aa299612acf3bc5733733036f520f2b6b97b8af0
nhn1030 <shgusska12@gmail.com>
2022-12-17 15:57:16 +0900

    Docs: 조건, 반복

---
+++ java-script/day2/nhn/factorial.js
@@ -0,0 +1,20 @@
+function facto(num) {
+    var res = 1;
+}
+
+
+
+
+function factorial(num) {
+    var result = 1;
+
+    for (var i = 1; i <= num; i++){
+        result *=i;
+    }
+    return result;
+}
+console.log(factorial(5))
+
+
+
+
